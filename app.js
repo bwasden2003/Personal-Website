675a93ddf47a69836331368ec225fb372a847cde
bwasden2003 <bwasden2003@gmail.com>
2023-06-08 15:00:21 -0500

    added about section

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+
 menu.addEventListener('click', function() {
 	menu.classList.toggle('is-active');
 	menuLinks.classList.toggle('active');
 });
+
+var tablinks = document.getElementsByClassName("tab__links");
+var tabcontents = document.getElementsByClassName("tab__contents");
+
+function opentab(tabname) {
+	for (tablink of tablinks) {
+		tablink.classList.remove("active__link");
+	}
+	for (tabcontent of tabcontents) {
+		tabcontent.classList.remove("active__tab");
+	}
+	event.currentTarget.classList.add("active__link")
+	document.getElementById(tabname).classList.add("active__tab")
+}
